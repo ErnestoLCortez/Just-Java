e7d1c7ddc4adb374c99ae06b84fccbb6e68996a3
Louie Cortez <ernestluiscortez@gmail.com>
2016-07-13 16:04:35 -0700

    Add email functionality

--- app/src/main/java/com/example/android/justjava/MainActivity.java
+++ app/src/main/java/com/example/android/justjava/MainActivity.java
@@ -1,11 +1,16 @@
 package com.example.android.justjava;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.CheckBox;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
 import java.text.NumberFormat;
 
 /**
@@ -24,14 +29,27 @@ public class MainActivity extends ActionBarActivity {
      * This method is called when the order button is clicked.
      */
     public void submitOrder(View view) {
+        EditText nameField = (EditText) findViewById(R.id.user_name);
+        String name = nameField.getText().toString();
+        Log.v("MainActivity", "Name: " + name);
+
         CheckBox whippedCreamCheckBox = (CheckBox) findViewById(R.id.whipped_cream);
         boolean hasWhippedCream = whippedCreamCheckBox.isChecked();
         Log.v("MainActivity","Has whipped cream: " + hasWhippedCream);
 
+        CheckBox chocolateCheckBox = (CheckBox) findViewById(R.id.chocolate_checkbox);
+        boolean hasChocolate = chocolateCheckBox.isChecked();
+        Log.v("MainActivity", "Has whipped cream: " + hasChocolate);
 
+        String priceMessage = createOrderSummary(name, calculatePrice(),hasWhippedCream, hasChocolate);
+        Intent intent = new Intent(Intent.ACTION_SENDTO);
+        intent.setData(Uri.parse("mailto:")); // only email apps should handle this
+        intent.putExtra(Intent.EXTRA_SUBJECT, "Just Java order for " + name);
+        intent.putExtra(Intent.EXTRA_TEXT, priceMessage);
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        }
 
-        String priceMessage = createOrderSummary(calculatePrice(),hasWhippedCream);
-        displayMessage(priceMessage);
     }
     /**
      * This method is called to calculate price.
@@ -43,13 +61,23 @@ public class MainActivity extends ActionBarActivity {
      * This method is called when the increment button is clicked.
      */
     public void increment(View view) {
-        displayQuantity(quantity+=1);
+        if (quantity < 100){
+            displayQuantity(quantity+=1);
+        }
+        else {
+            Toast.makeText(this, "You cannot have more than 100 coffees", Toast.LENGTH_SHORT).show();
+        }
     }
     /**
      * This method is called when the decrement button is clicked.
      */
     public void decrement(View view) {
-        displayQuantity(quantity-=1);
+        if (quantity > 1){
+            displayQuantity(quantity-=1);
+        }
+        else {
+            Toast.makeText(this, "You cannot have less than 1 coffee", Toast.LENGTH_SHORT).show();
+        }
     }
     /**
      * This method displays the given quantity value on the screen.
@@ -59,17 +87,13 @@ public class MainActivity extends ActionBarActivity {
                 R.id.quantity_text_view);
         quantityTextView.setText("" + number);
     }
-    /**
-     * This method displays the given text on the screen.
-     */
-    private void displayMessage(String message) {
-        TextView orderSummaryTextView = (TextView) findViewById(R.id.order_summary_text_view);
-        orderSummaryTextView.setText(message);
-    }
+
     /**
      * This method displays order summary to screen.
      */
-    private String createOrderSummary(int price, boolean addWhippedCream) {
-        return "Name: Kaptain Kunal \n Add Whipped Cream? " + addWhippedCream + "\nQuantity: " + quantity + "\nTotal: $" + price + "\nThank you!";
+    private String createOrderSummary(String name, int price, boolean addWhippedCream, boolean addChocolate) {
+
+        return "Name: " + name +  "\nAdd Whipped Cream? " + addWhippedCream  +
+                "\nAdd Chocolate? " + addChocolate + "\nQuantity: " + quantity + "\nTotal: $" + price + "\nThank you!";
     }
 }
